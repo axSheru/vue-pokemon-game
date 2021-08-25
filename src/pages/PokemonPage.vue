6930ef54447ea23238b20712ed7b14ecfa4c1d89
Alex Paredes <alexparedesr95@gmail.com>
2021-08-25 00:11:05 -0500

    Se pasa el arreglo de Pokémon al PokemonPage y se listan.

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>¿Quién es este Pokémon?</h1>
   <PokemonPicture :pokemonId=4 :showPokemon="false" />
-  <PokemonOptions/>
+  <PokemonOptions :pokemons="pokemonArr" />
 </template>
 
 <script>
@@ -15,6 +15,19 @@ export default {
     components: {
         PokemonPicture,
         PokemonOptions
+    },
+    data() {
+      return {
+        pokemonArr: []
+      }
+    },
+    methods: {
+      async mixPokemonArr() {
+        this.pokemonArr = await getPokemonOptions()
+      }
+    },
+    mounted() {
+      this.mixPokemonArr()
     }
 
 }
