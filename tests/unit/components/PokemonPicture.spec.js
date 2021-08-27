e9cf212c514af8d2fe060d9b2591d30899c4b953
Alex Paredes <alexparedesr95@gmail.com>
2021-08-26 23:42:45 -0500

    Se definen tests a realizar en PokemonPicture.

--- tests/unit/components/PokemonPicture.spec.js
+++ tests/unit/components/PokemonPicture.spec.js
@@ -6,6 +6,13 @@ describe('PokemonPicture component', () => {
     test('Debe de hacer match con el snapshot.', () => {
         
     })
-    
 
+    test('Debe de mostrar la imagen oculta y el pokemon 100', () => {
+        
+    })
+
+    test('Debe de mostrar el Pokémon si showPokemon:true', () => {
+        
+    })
+    
 })
