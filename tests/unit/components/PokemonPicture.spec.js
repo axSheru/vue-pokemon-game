d0f3a5a8aa47a4995b0fe278bf6eb5a5bac9daba
Alex Paredes <alexparedesr95@gmail.com>
2021-08-26 23:42:14 -0500

    Se crea el archivo para pruebas de PokemonPicture. Se definen tests a realizar para PokémonPicture.

---
+++ tests/unit/components/PokemonPicture.spec.js
@@ -0,0 +1,11 @@
+import { shallowMount } from '@vue/test-utils';
+import PokemonPicture from '@/components/PokemonPicture';
+
+describe('PokemonPicture component', () => {
+
+    test('Debe de hacer match con el snapshot.', () => {
+        
+    })
+    
+
+})
