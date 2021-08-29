d01548b3b6274c8afc5dcfab24d8b66fb7d2c3c1
Alex Paredes <alexparedesr95@gmail.com>
2021-08-29 00:34:10 -0500

    FEAT: Se realiza test para validar que la cantidad de etiquetas li existentes y su contenido.

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -16,6 +16,24 @@ describe("PokemonOptions Component.", () => {
   test("Debe de hacer match con el snapshot.", () => {
 
     expect(wrapper.html()).toMatchSnapshot()
-    
+
   });
+
+  test('Debe de mostrar las 4 opciones correctamente.', () => {
+
+    const liTags = wrapper.findAll( 'li' )
+
+    expect( liTags.length ).toBe( 4 )
+
+    expect( liTags[0].text() ).toBe( 'bulbasaur' )
+    expect( liTags[1].text() ).toBe( 'ivysaur' )
+    expect( liTags[2].text() ).toBe( 'venusaur' )
+    expect( liTags[3].text() ).toBe( 'charmander' )
+      
+  })
+
+  test('Debe de emitir "selection" con sus respectivos parámetros al hacer clic.', () => {
+      
+  })
+
 });
