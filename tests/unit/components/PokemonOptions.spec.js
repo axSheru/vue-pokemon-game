91dd1803b2e407346316025d9528599bf6b30a28
Alex Paredes <alexparedesr95@gmail.com>
2021-08-29 00:45:14 -0500

    FEAT: Se realiza test para evaluar los emits del PokemonOptions component.

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -33,6 +33,19 @@ describe("PokemonOptions Component.", () => {
   })
 
   test('Debe de emitir "selection" con sus respectivos parámetros al hacer clic.', () => {
+
+    const [ li1, li2, li3, li4 ] = wrapper.findAll( 'li' )
+
+    li1.trigger( 'click' )
+    li2.trigger( 'click' )
+    li3.trigger( 'click' )
+    li4.trigger( 'click' )
+
+    expect( wrapper.emitted( 'selection' ).length ).toBe( 4 )
+    expect( wrapper.emitted( 'selection' )[0] ).toEqual( [1] )
+    expect( wrapper.emitted( 'selection' )[1] ).toEqual( [2] )
+    expect( wrapper.emitted( 'selection' )[2] ).toEqual( [3] )
+    expect( wrapper.emitted( 'selection' )[3] ).toEqual( [4] )
       
   })
 
