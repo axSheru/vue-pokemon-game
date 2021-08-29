8d32855b31887cc2b663d72a635c7b2bc604736e
Alex Paredes <alexparedesr95@gmail.com>
2021-08-29 00:20:43 -0500

    FEAT: Se realiza test de snapshot en el componente PokemonOptions.

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -1,6 +1,21 @@
-import { shallowMount } from '@vue/test-utils';
-import { PokemonOptions } from '@/components/PokemonOptions';
+import { shallowMount } from "@vue/test-utils";
+import PokemonOptions from "@/components/PokemonOptions";
+import { mockPokemons } from "../Mocks/Pokemons.mock.js";
 
-describe('PokemonOptions Component.', () => {
+describe("PokemonOptions Component.", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(PokemonOptions, {
+      props: {
+        pokemons: mockPokemons
+      }
+    });
+  });
+
+  test("Debe de hacer match con el snapshot.", () => {
+
+    expect(wrapper.html()).toMatchSnapshot()
     
-})
+  });
+});
