ea33b5ec23b222e9a87487e674b6b5d19e3bf2ee
Alex Paredes <alexparedesr95@gmail.com>
2021-08-30 23:05:28 -0500

    FEAT: Se realiza test para hacer match con el snapshot. Se realiza test para verificar que el método mixPokemonArr se manda a llamar antes de montar el PokemonPage Component.

--- tests/unit/pages/PokemonPage.spec.js
+++ tests/unit/pages/PokemonPage.spec.js
@@ -1,7 +1,26 @@
-import { shallowMount, mount } from '@vue/test-utils';
+import { shallowMount, mount } from '@vue/test-utils'
+import PokemonPage from '@/pages/PokemonPage'
 
 describe('PokemonPage Component', () => {
-    
+
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallowMount( PokemonPage )
+    })
+
+    test('Debe de hacer match con el snapshot', () => {        
+        expect( wrapper.html() ).toMatchSnapshot()
+    })
+
+    test('Debe de llamar mixPokemonArr cuando se monta.', () => {
+        
+        const mixPokemonArraySpy = jest.spyOn( PokemonPage.methods, 'mixPokemonArr' )
+        shallowMount( PokemonPage )
+
+        expect( mixPokemonArraySpy ).toHaveBeenCalled()
+
+    })
     
     
 
